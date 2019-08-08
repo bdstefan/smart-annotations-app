a4e052b5cd90287ef299f21773586611934fbf08
BDS <dumitru.boila@gmail.com>
2019-08-08 21:57:33 +0300

    Added a helper bash script

---
+++ api.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#curl -X POST -d "comment=This a new annotation" -H "Authorization: Basic YWRtaW46c3VwZXJzZWNyZXQ" -H "Accept: application/json" http://localhost:3000/annotations/
+#curl -X GET -H "Authorization: Basic YWRtaW46c3VwZXJzZWNyZXQ" -H "Accept: application/json" http://localhost:3000/annotations/
+curl -X PATCH -d "comment=This an updated annotation v3" -H "Authorization: Basic YWRtaW46c3VwZXJzZWNyZXQ" -H "Accept: application/json" http://localhost:3000/annotations/5d4c6cc542882b02e50863fb
+#curl -X DELETE -H "Authorization: Basic YWRtaW46c3VwZXJzZWNyZXQ" -H "Accept: application/json" http://localhost:3000/annotations/5d4c6cc542882b02e50863fb
